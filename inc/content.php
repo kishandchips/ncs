b11c60c4e943e722da1dfaab325df1961934633f
Franszo <josh@kishandchips.com>
2014-07-31 12:35:42 +0100

    Expertise content Layouts

--- inc/content.php
+++ inc/content.php
@@ -416,10 +416,33 @@
 </div>	
 <?php endif; ?>
 
+<?php
+/****************************************
+ * Expertise Pages
+****************************************/
+?>
+<?php if(get_field('rows', $id)): ?>
+	<div id="expertise">
+		<h1 class="section-title"><?php the_field('sections_title'); ?></h1>
+		<?php while( have_rows('rows') ): the_row(); ?>		
+			<div class="row <?php if (get_sub_field('content_alignment')):?>right<?php endif; ?>" <?php if (get_sub_field('background')):?>style="background-image: url('<?php the_sub_field('background'); ?>');"<?php endif; ?>>
+				<div class="span <?php if (get_sub_field('full_width')):?>ten<?php else: ?>four<?php endif; ?>" style="<?php if (get_sub_field('content_background_color')):?>background-color: <?php the_sub_field('content_background_color'); ?>;<?php endif; ?><?php if (get_sub_field('content_color')):?>color: <?php the_sub_field('content_color'); ?>;<?php endif; ?>">
+					<?php the_sub_field('content'); ?>
+					<?php if (get_sub_field('box_link')): ?>
+						<a class="more-about" href="<?php the_sub_field('box_link') ?>"><?php _e('Read More'); ?></a>
+					<?php endif; ?>
+					
+				</div>
+			</div>
+		<?php endwhile; ?>
+	</div>
+
+<?php endif; ?>
+
 
 <?php
 /****************************************
- * Environmental Policy
+ * Articles & Related Case Studies
 ****************************************/
 ?>
 <?php if(get_field('show_related_case_studies', $id)): ?>
@@ -456,3 +479,5 @@
 			</div>
 	<?php endif; ?>
 <?php endif; ?>
+
+
